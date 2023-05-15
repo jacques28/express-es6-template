639b97252157d34fbb711d43268895ce33b5dc20
jacques28 <devjacques28i@gmail.com>
2023-05-15 19:39:56 -0400

    Add: dotenv files body-parser cors

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+import bodyParser from 'body-parser';
+import 'dotenv/config.js'
+import cors from 'cors';
+
+
 const app = express();
 
+app.use(bodyParser.json())
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
 
 app.get("/", (req, res) => {
     res.send("hello");
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 ### Express app with js ES6 module
 #### The bin file not used !
+### .env 
+    ``CORS_ORIGIN=localhost:***``
+    
